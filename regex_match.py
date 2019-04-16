64dc1bef3bf7b5101615b79082d10339e0a37c25
Jeromie Kirchoff <jahomie04@gmail.com>
2019-04-15 22:19:30 -0700

    Update to code and comment
    More work in progress but goal is to find random email addresses in random batches of text.

--- regex_match.py
+++ regex_match.py
@@ -5,14 +5,15 @@
 # Created By  : Jeromie Kirchoff
 # Created Date: Mon August 15 22:00:03 PDT 2018
 # =============================================================================
-"""THE MODULE HAS BEEN BUILD FOR."""
+"""THE MODULE HAS BEEN BUILD TO TRY AND FIND EMAIL ADDRESSES IN RANDOM TEXT."""
 # =============================================================================
 # IMPORTS
 # =============================================================================
 import re
 
-str = 'purple asdf-b@google.com monkey spacewasher'
+str = 'purple asdf-b@google.com monkey spacewasher Something (at) Yahoo (Dot com'
 match = re.search(r'([\w.-]+)@([\w.-]+)', str)
+# match = re.search(r'([\w.-]+)\s?\(?(at|AT)\)?\s?([\w.-]+)\s?\(?\s?(dot|DOT)\s?\)?\s?\(?(com|COM|us|US|nz|NZ|de|DE|uk|UK)\s?\)?\s?', str)
 if match:
     print(match.group())   # 'asdf-b@google.com' (the whole match)
     print(match.group(1))  # 'asdf-b' (the username, group 1)
