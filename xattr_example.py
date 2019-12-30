5d227d9928600f774e8bbf243217caa92767cf13
JayRizzo <Jahomie04@gmail.com>
2019-12-30 00:48:34 -0800

    converting xattr to class
    and to showcase using self when calling directly.

--- xattr_example.py
+++ xattr_example.py
@@ -9,55 +9,93 @@ Showcasing a 'How-to' example.
 # =============================================================================
 
 import xattr
-
-print("{}".format(xattr.__file__))
-# '/usr/local/lib/python3.7/site-packages/xattr/__init__.py'
+from getpass import getuser
 
 
-def showww_me_the_meta(file_name):
-    """Using Python's XATTR to list Key Meta Names for File."""
-    print("Showing Initial Names & Values.")
-    attrz = xattr.listxattr(file_name)
-    result = ("A. Info Showcased Init: {}".format(attrz))
-    print("{}".format(result))
-    return result
+class MetaModz(object):
+    """Docstring for MetaModz."""
 
+    def __init__(self, arg):
+        """Docstring for MetaModz.__init__."""
+        super(MetaModz, self).__init__()
+        self.file_name = arg.__str__()
+        self.attrz = []
+        print("\n\nRunning File: {}\n".format(self.file_name))
+        # '/usr/local/lib/python3.7/site-packages/xattr/__init__.py'
 
-def update_the_meta(file_name):
-    """Using Python's XATTR to Update Key Meta Names for File."""
-    xattr.setxattr(file_name, 'custom.comment',
-                   'I tawt I taw a puddy tat!.'.encode('utf-8'))
-    xattr.setxattr(file_name, 'Music.Artist',
-                   'I did! '
-                   'I did taw a puddy tat!'.encode('utf-8'))
-    get_the_meta_values(file_name)
-    return
+    def showww_me_the_meta(self):
+        """Using Python's XATTR to list Key Meta Names for File."""
+        attrz = xattr.listxattr(self.file_name)
+        print("List All Meta Key's: {}".format(attrz))
+        return attrz
 
+    def update_the_meta(self):
+        """Using Python's XATTR to Update Key Meta Names for File."""
+        xattr.setxattr(self.file_name, 'custom.comment',
+                       'I tawt I taw a puddy tat!.'.encode('utf-8'))
+        xattr.setxattr(self.file_name, 'Music.Artist',
+                       'I did! '
+                       'I did taw a puddy tat!'.encode('utf-8'))
+        print("Update Meta Key's Names for File: {}".format(self.file_name))
+        return
 
-def get_the_meta_values(file_name):
-    """Example Looping thru keys to get the values."""
-    print("B. Listing Meta for: {}".format(file_name))
-    attrz = xattr.listxattr(file_name)
-    print("")
-    for i in reversed(attrz):
-        abc = xattr.getxattr(file_name, i)
-        result = ("{} : {}".format(i, abc))
-        print("   {}".format(result))
-    print("")
-    return
+    def update_file_owner(self):
+        """Using Python's XATTR to Update Key Meta Names with owner info."""
+        xattr.setxattr(self.file_name, 'custom.owner',
+                       "{}".format(getuser()).encode('utf-8'))
+        print("Update Meta Key's Names for File: {}".format(self.file_name))
+        return
 
+    def get_the_meta_values(self):
+        """Example Looping thru keys to get the values."""
+        print("Get The Meta for: {}".format(self.file_name))
+        attrz = xattr.listxattr(self.file_name)
+        print("")
+        for i in reversed(attrz):
+            abc = xattr.getxattr(self.file_name, i)
+            result = ("{} : {}".format(i, abc))
+            print("   {}".format(result))
+        print("")
+        return attrz
 
-def remove_the_meta(file_name):
-    """Example of removing the keys added to the file."""
-    xattr.removexattr(file_name, 'custom.comment')
-    xattr.removexattr(file_name, 'Music.Artist')
-    attrz = xattr.listxattr(file_name)
-    result = ("C. Info Removed Meta: {}".format(attrz))
-    print("{}".format(result))
-    return result
+    def remove_the_meta(self):
+        """Example of removing the keys added to the file."""
+        print("Delete The Meta.")
+        xattr.removexattr(self.file_name, 'custom.comment')
+        attrz = xattr.listxattr(self.file_name)
+        result = ("D. Info Removed Meta: {}".format(attrz))
+        print("{}".format(result))
+        print("The Meta Has Been Removed.")
+        return result
+
+    def remove_all_the_meta(self):
+        """Example of removing the keys added to the file."""
+        print("Deleting All Meta.")
+        attrz = xattr.listxattr(self.file_name)
+        for i in reversed(attrz):
+            xattr.removexattr(self.file_name, i)
+            print("    {}: Removed".format(i))
+        print("")
+        print("All Meta Has Been Removed.")
+        return
 
 
 if __name__ == '__main__':
-    showww_me_the_meta('xattr_example.py')
-    update_the_meta('xattr_example.py')
-    remove_the_meta('xattr_example.py')
+    # use this to test
+    a = MetaModz(xattr.__file__)
+
+    # use this to select a file
+    # a = MetaModz(input("Please Provide Absolute Path: "))
+
+    # use this to explicitly run against a defined file.
+    # a = MetaModz('~/Desktop/test.text')
+
+    a.showww_me_the_meta()
+    a.update_the_meta()
+    a.update_file_owner()
+    a.showww_me_the_meta()
+    a.get_the_meta_values()
+    a.remove_the_meta()
+    a.showww_me_the_meta()
+    a.remove_all_the_meta()
+    a.showww_me_the_meta()
